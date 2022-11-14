c51a2e065bcbc874f03817558fb7a95ef163f747
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-11-14 18:15:38 -0500

    Update TruflationCrudeOilMumbai.sol

--- contracts/TruflationCrudeOilMumbai.sol
+++ contracts/TruflationCrudeOilMumbai.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.17;
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
@@ -6,12 +7,10 @@ import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
 contract TruflationCrudeOilMumbai is ChainlinkClient {
 
-    bytes   public result;
+    uint public constant fee = 1 ether;   //1 LINK TOKEN
+    uint public result;
     address public constant oracleId = 0x6D141Cf6C43f7eABF94E288f5aa3f23357278499; //MUMBAI 
-    string  public  constant  jobId = "d220e5e687884462909a03021385b7ae"; //MUMBAI
-    uint256 public constant fee = 1 ether;   //1 LINK TOKEN
-
-    mapping(bytes32 => bytes) public results;
+    string  public constant  jobId = "d220e5e687884462909a03021385b7ae"; //MUMBAI
 
     using Chainlink for Chainlink.Request;
 
@@ -25,25 +24,12 @@ contract TruflationCrudeOilMumbai is ChainlinkClient {
         req.add("data", "{'id':'8002050'}");
         req.add("keypath", "value");
         req.add("abi", "uint256");
-        req.add("multiplier", "1000000000000000000");
+        req.add("multiplier", "10000000000000000000");
         return sendChainlinkRequestTo(oracleId, req, fee);
     }
 
-    function fulfillBytes(bytes32 _requestId, bytes memory bytesData)
-        public recordChainlinkFulfillment(_requestId) {
-        result = bytesData;
-        results[_requestId] = bytesData;
+    function fulfillBytes(bytes32 _requestId, uint uintData) public recordChainlinkFulfillment(_requestId) {
+        result = uintData;
     }
-
-    function getInt256(bytes32 _requestId) public view returns (int256) {
-       return toInt256(results[_requestId]);
-    }
-
-    function toInt256(bytes memory _bytes) internal pure
-      returns (int256 value) {
-          assembly {
-            value := mload(add(_bytes, 0x20))
-      }
-   }
 
 }
