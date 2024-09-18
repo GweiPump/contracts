a0cfbe5a7a29303246232173159d9d78e6fd83ed
MarcusWentz <markzcollege404@gmail.com>
2024-09-18 16:12:23 -0400

    Chainlink VRF Foundry library compile

--- src/VockTails.sol
+++ src/VockTails.sol
@@ -1,36 +1,39 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity 0.8.26;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
 
 // import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import {LinkTokenInterface} from "chainlink/v0.8/interfaces/LinkTokenInterface.sol"; 
 // import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFCoordinatorV2Interface} from "chainlink/v0.8/interfaces/VRFCoordinatorV2Interface.sol"; 
 // import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import {VRFConsumerBaseV2} from "chainlink/v0.8/VRFConsumerBaseV2.sol"; 
 
-// contract VRFv2LightShow is VRFConsumerBaseV2 {
-//   VRFCoordinatorV2Interface COORDINATOR;
-//   LinkTokenInterface LINKTOKEN;
+contract VRFv2LightShow is VRFConsumerBaseV2 {
+  VRFCoordinatorV2Interface COORDINATOR;
+  LinkTokenInterface LINKTOKEN;
 
-//   uint256[] public twoRandomWords;
-//   uint256 requestId; 
-//   event lightShowUpdate();
+  uint256[] public twoRandomWords;
+  uint256 requestId; 
+  event lightShowUpdate();
 
-//   constructor() VRFConsumerBaseV2(0x6168499c0cFfCaCD319c818142124B7A15E857ab) {
-//     COORDINATOR = VRFCoordinatorV2Interface(0x6168499c0cFfCaCD319c818142124B7A15E857ab);
-//     LINKTOKEN = LinkTokenInterface(0x01BE23585060835E02B77ef475b0Cc51aA1e0709);
-//   }
+  constructor() VRFConsumerBaseV2(0x6168499c0cFfCaCD319c818142124B7A15E857ab) {
+    COORDINATOR = VRFCoordinatorV2Interface(0x6168499c0cFfCaCD319c818142124B7A15E857ab);
+    LINKTOKEN = LinkTokenInterface(0x01BE23585060835E02B77ef475b0Cc51aA1e0709);
+  }
 
-//   function requestRandomWords() external {
-//       requestId = COORDINATOR.requestRandomWords(
-//       0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc, //keyHash
-//       529,    //subscriptionId [Get your ID here https://vrf.chain.link/]
-//       3,      //Confirmations
-//       100000, //callbackGasLimit
-//       2       //numWords
-//     );
-//   }
+  function requestRandomWords() external {
+      requestId = COORDINATOR.requestRandomWords(
+      0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc, //keyHash
+      529,    //subscriptionId [Get your ID here https://vrf.chain.link/]
+      3,      //Confirmations
+      100000, //callbackGasLimit
+      2       //numWords
+    );
+  }
   
-//   function fulfillRandomWords(uint256,  uint256[] memory randomWords) internal override {
-//     twoRandomWords = randomWords;
-//     emit lightShowUpdate();
-//   }
+  function fulfillRandomWords(uint256,  uint256[] memory randomWords) internal override {
+    twoRandomWords = randomWords;
+    emit lightShowUpdate();
+  }
 
-// }
+}
